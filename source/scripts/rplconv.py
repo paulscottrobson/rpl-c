466943788d55411389492cc31d9c74b259babf3a
paulscottrobson <paulscottrobson@gmail.com>
2020-01-10 08:16:23 +0000

    Variable handler decoders written

--- source/scripts/rplconv.py
+++ source/scripts/rplconv.py
@@ -162,14 +162,14 @@ class LineConverter(object):
 			self.code += wByte
 			return nextCode
 		#		
-		if re.match("^[A-Z][A-Z0-9]*[\\@\\!\\&]$",word) is not None:			# is it var@ var!
+		if re.match("^[A-Z][A-Z0-9]*[\\@\\!\\&]$",word) is not None:		# is it var@ var! var&
 			if len(word) > 4:
 				raise ConversionException("Variable too long "+word)
 			name = word[:-1]+"  "											# pad name out.
 			nameEnc = self.getCh(name[0])+self.getCh(name[1])*32+self.getCh(name[2])*32*40
 			self.appendWord("$$"+word[-1]+"handler")
-			self.code.append(nameEnc >> 8)
 			self.code.append(nameEnc & 0xFF)
+			self.code.append(nameEnc >> 8)
 			return nextCode
 		#
 		raise ConversionException("Can't process "+word)
